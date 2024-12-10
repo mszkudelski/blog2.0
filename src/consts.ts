d112bb4d3695c4cf7320c71425d86e1e038bcc25
Marek Szkudelski <marek.szkudelski@gmail.com>
2024-12-10 14:09:32 +0100

    Enhance email protection component; update email format and add Google Tag integration

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@ import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
   NAME: "Marek Szkudelski",
-  EMAIL: "marek.szkudelski@gmail.com",
+  EMAIL: "marek@szkudelski.dev",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
